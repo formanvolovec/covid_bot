cc69b216337d267312534a57d9c8a7a4a2b186f8
formanvolovec <formanvolovec@gmail.com>
2020-04-04 13:46:17 +0300

    add buttonst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import COVID19Py
 import telebot
+from telebot import types
+import COVID19Py
 
 covid19 = COVID19Py.COVID19()
 bot = telebot.TeleBot('1227263304:AAHlmuYYa6LJdXFBYDreNAV-yAgQPz6WxDo')
@@ -7,8 +8,20 @@ bot = telebot.TeleBot('1227263304:AAHlmuYYa6LJdXFBYDreNAV-yAgQPz6WxDo')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    send_mess = f"<b>Привет {message.from_user.first_name}</b>\nВведите страну"
-    bot.send_message(message.chat.id, send_mess, parse_mode='html')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton('Во всём мире')
+    btn2 = types.KeyboardButton('Сша')
+    btn3 = types.KeyboardButton('Россия')
+    btn4 = types.KeyboardButton('Беларусь')
+    btn5 = types.KeyboardButton('Украина')
+    btn6 = types.KeyboardButton('Италия')
+    btn7 = types.KeyboardButton('Испания')
+    btn8 = types.KeyboardButton('Германия')
+    markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8)
+
+    send_message = f"<b>Привет {message.from_user.first_name}!</b>\nЧтобы узнать данные про коронавируса " \
+                   f"напишите название страны.\n"
+    bot.send_message(message.chat.id, send_message, parse_mode='html', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
@@ -23,19 +36,15 @@ def mess(message):
         location = covid19.getLocationByCountryCode("RU")
     elif get_message_bot == "беларусь":
         location = covid19.getLocationByCountryCode("BY")
-    elif get_message_bot == "казакхстан":
-        location = covid19.getLocationByCountryCode("KZ")
     elif get_message_bot == "италия":
         location = covid19.getLocationByCountryCode("IT")
-    elif get_message_bot == "франция":
-        location = covid19.getLocationByCountryCode("FR")
+    elif get_message_bot == "испания":
+        location = covid19.getLocationByCountryCode("ES")
     elif get_message_bot == "германия":
         location = covid19.getLocationByCountryCode("DE")
-    elif get_message_bot == "катай":
-        location = covid19.getLocationByCountryCode("CN")
     else:
         location = covid19.getLatest()
-    final_message = f"<u>Данные по всему миру:</u>\n<b>Заболевших: </b>{location['confirmed']:,}\n<b>Сметрей: </b>{location['deaths']:,}"
+        final_message = f"<u>Данные по всему миру:</u>\n<b>Заболевших: </b>{location['confirmed']:,}\n<b>Сметрей: </b>{location['deaths']:,}"
 
     if final_message == "":
         date = location[0]['last_updated'].split("T")
